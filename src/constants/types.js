ce681340e6b5eef290c02b2ff460de101aad9874
Samet Alptekin Eroğlu <sametalptekin.eroglu@gmail.com>
2025-04-17 22:31:13 +0300

    hw3-crud

--- src/constants/types.js
+++
@@ -1,9 +0,0 @@
-// enum: ['work', 'home', 'personal'],
-
-const TYPES = {
-  WORK: 'work',
-  HOME: 'home',
-  PERSONAL: 'personal',
-};
-
-export default TYPES;
--- src/routers/root.js
+++ src/routers/root.js
@@ -1,11 +1,12 @@
-import { Router } from 'express';
+import { Router } from "express";
 
 const rootRouter = Router();
 
-rootRouter.get('/', (req, res) => {
+rootRouter.get("/", (req, res) => {
   res.status(200).json({
     status: 200,
-    message: 'Hello World'
+    message: "Hello World",
+    data: {},
   });
 });
 

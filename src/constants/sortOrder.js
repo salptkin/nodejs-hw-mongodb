161626d6952dd445968f0df221d4122120454638
Samet Alptekin Eroğlu <sametalptekin.eroglu@gmail.com>
2025-05-06 21:37:53 +0300

    auth

--- src/constants/sortOrder.js
+++ src/constants/sortOrder.js
@@ -1,10 +1,11 @@
-// src/constants/sortOrder.js
-
 const SORT_ORDER = {
   ASC: 'asc',
   ASCENDING: 'asc',
   DESC: 'desc',
   DESCENDING: 'desc',
 };
+
+export const FIFTEEN_MINUTES = 15 * 60 * 1000;
+export const THIRTY_DAY = 30 * (24 * 60 * 60 * 1000);
 
 export default SORT_ORDER;

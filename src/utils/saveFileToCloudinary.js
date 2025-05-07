a8f24e387b6a4814377474cacb50b2883b1018de
Samet Alptekin Eroğlu <sametalptekin.eroglu@gmail.com>
2025-05-08 01:33:40 +0300

    Cloudinary Save Filev3

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -1,18 +1,13 @@
+/* eslint-disable no-undef */
 import cloudinary from 'cloudinary';
-import fs from 'fs/promises';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-cloudinary.v2.config({
-  secure: true,
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
 
 export const saveFileToCloudinary = async (file) => {
+  cloudinary.v2.config({
+    secure: true,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
   const response = await cloudinary.v2.uploader.upload(file.path);
-  await fs.unlink(file.path);
   return response.secure_url;
 };
